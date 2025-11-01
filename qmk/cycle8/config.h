0d844e1654bcc3714a97f7e10d0d999ce8083471
root <root@dockerhost.rowra.org>
2025-11-01 22:22:15 +0100

    added qmk files

---
+++ qmk/cycle8/config.h
@@ -0,0 +1,7 @@
+// Copyright 2022 Eason
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define WS2812_SPI_DRIVER SPID2
+#define LED_PIN_ON_STATE 0
---
+++ qmk/cycle8/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9D, 0x52, 0x08, 0x2F, 0xC6, 0x38, 0x4F, 0x6D}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
